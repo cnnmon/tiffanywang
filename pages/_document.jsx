726217374461b1f7d38a722e1da8669f467ff7d1
cnnmon <cnnmonn@gmail.com>
2022-11-01 15:16:35 -0700

    crossorigin

--- pages/_document.jsx
+++ pages/_document.jsx
@@ -9,7 +9,7 @@ class MyDocument extends Document {
       <Html>
         <Head>
           <link rel="preconnect" href="https://fonts.googleapis.com" />
-          <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+          <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
           <link href="https://fonts.googleapis.com/css2?family=IBM+Plex+Mono:wght@300;700&display=swap" rel="stylesheet" />
         </Head>
         <body>
