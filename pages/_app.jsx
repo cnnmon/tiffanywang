d78eec302256b58511e08f784d4335f40bfac088
cnnmon <cnnmonn@gmail.com>
2023-02-24 13:50:10 -0800

    navlinks

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -3,12 +3,13 @@ import logo from '../public/logo.svg';
 import Image from 'next/image';
 import Link from 'next/link';
 import Head from 'next/head';
+import { useRouter } from 'next/router';
 
 function MyApp({ Component, pageProps }) {
-  function NavItem({ path, title, name }) {
-    const isActive = Component.name == name;
+  function NavItem({ path, title }) {
+    const router = useRouter();
+    const isActive = router.pathname === path;
 
-    // TODO: navigation styling for active link doesn't work on deployment
     return (
       <Link href={path} className={isActive ? 'font-bold text-cosmic' : ''}>
         <span className="hidden md:inline">
@@ -27,9 +28,9 @@ function MyApp({ Component, pageProps }) {
       <Image src={logo} alt="logo" className="w-[150px] md:w-1/5 px-6" />
       <div className="flex md:flex-row flex-col">
         <nav className="flex md:w-1/5 p-3 md:flex-col gap-2 text-right">
-          <NavItem path="/" title="home" name="Home" />
-          <NavItem path="/about" title="about" name="About" />
-          <NavItem path="/projects" title="projects" name="Projects" />
+          <NavItem path="/" title="home" />
+          <NavItem path="/about" title="about" />
+          <NavItem path="/projects" title="projects" />
           <a href="https://drive.google.com/file/d/1eLelGRhX6h65sQhYiB_LqCli2UL2U-wW/view" target="_blank">resume</a>
         </nav>
         <div className="md:w-4/5 p-3 body">
