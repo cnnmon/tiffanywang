1c9540b6eefa9d5426587441d68a3d2311c1f660
cnnmon <cnnmonn@gmail.com>
2023-02-24 21:52:06 -0800

    404

--- pages/404.jsx
+++ pages/404.jsx
@@ -1,7 +1,7 @@
 function NotFound() {
   return (
     <div>
-      <p className="font-bold">404 not found. Turn back!</p>
+      <p className="font-bold">404 not found.</p> Turn back!
     </div>
   );
 }
