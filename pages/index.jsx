d291378e69104a858176cfa6543abde14c711290
cnnmon <tiffanywang@berkeley.edu>
2025-05-10 21:26:17 -0700

    wip

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,77 +1,55 @@
-import { useEffect, useState } from 'react'
 import Url from '../components/Url'
+import { useState, useEffect } from 'react'
 
 const words = {
-  drinks: ['yakult', 'grape soda', 'oat matcha lattes', 'La Croix', 'Cholula hot sauce'],
-  hobbies: ['hosting social deduction board game nights', 'singing my heart out at karaoke', 'brewing the spiciest soups known to man', 'talking about the idea of going to the gym']
+  drinks: ['yakult 🍓', 'grape soda 🍇', 'oat matcha lattes 🍵', 'mango milk tea 🥭', 'Cholula hot sauce 🌶️'],
+  hobbies: ['host social deduction board game nights 🎲', 'sing my heart out at karaoke 🎤', 'brew the spiciest soups known to man 🍲', 'talk about the idea of going to the gym 💪']
 }
-const drinksEmoji = ['🍓', '🍇', '🍵', '🍊', '🌶️']
-const greeting = 'hi, i\'m tiffany!'
+
+function WaveText({ text }) {
+  return (
+    <span className="wave">
+      {text.split('').map((letter, index) => (<span key={index}>{letter == ' ' ? '\u00A0' : letter}</span>))}
+    </span>
+  )
+}
 
 function Home() {
-  // generate initial state for words dictionary
-  const initialWordsState = Object.fromEntries(Object.keys(words).map(key => [key, 0]))
-  const [wordsState, setWordsState] = useState(initialWordsState)
+  const [drinkIdx, setDrinkIdx] = useState(0)
+  const [hobbyIdx, setHobbyIdx] = useState(0)
 
   useEffect(() => {
-    // set drink to random
-    const randomDrinkIndex = Math.floor(Math.random() * words.drinks.length)
-    const randomHobbyIndex = Math.floor(Math.random() * words.hobbies.length)
-    setWordsState({ ...wordsState, drinks: randomDrinkIndex, hobbies: randomHobbyIndex })
+    setDrinkIdx(Math.floor(Math.random() * words.drinks.length))
+    setHobbyIdx(Math.floor(Math.random() * words.hobbies.length))
   }, [])
-  
-  let greetingCharacters = []
-  for (let i = 0; i < greeting.length; i++) {
-    const char = greeting[i]
-    greetingCharacters.push(char == ' ' ? '\u00A0' : char)
-  }
-
-  function incrementWords(key) {
-    const index = wordsState[key]
-    const length = words[key].length
-    const nextIndex = (index + 1) % length
-    setWordsState({...wordsState, [key]: nextIndex})
-  }
-
-  function PlayLink({ type }) {
-    return (
-      <a className="link play" onClick={() => incrementWords(type)}>
-        {words[type][wordsState[type]]}
-      </a>
-    )
-  }
 
   return (
-    <>
-      <h1 className="pt-10 pb-5 text-center">
-        <span className="wave">
-          {greetingCharacters.map((letter, index) => (<span key={index}>{letter}</span>))}
-        </span>
-      </h1>
+    <div className="flex flex-col text-lg select-none">
+      <p>
+        <WaveText text="welcome to my web home" />!
+        {" "}im tiffany. go ahead, grab yourself a drink; my cooler stocks infinite amounts of <a className="link play" onClick={() => setDrinkIdx((prev) => (prev + 1) % words.drinks.length)}>{words.drinks[drinkIdx]}</a>.
+      </p>
+      
+      <br />
+      <p>im based in sunny san francisco, where i:</p>
+      <p>¹work on <Url href="https://mj-storytelling.github.io/">ai-supported creative tools</Url></p>
+      <p>²make experimental <Url href="/projects">indie games</Url></p>
+      <p>³<a className="link play" onClick={() => setHobbyIdx((prev) => (prev + 1) % words.hobbies.length)}>{words.hobbies[hobbyIdx]}</a></p>
+
       <br />
       <p>
-        ⋆✩˚.⋆. <br /><br />
-        <b>Welcome to my web-home!</b> Go ahead, grab yourself something to drink. I have a cooler full of{' '}
-        <PlayLink type="drinks" />
-        {' '}{drinksEmoji[wordsState['drinks']]}.
-        <br /><br />
-        I'm a programmer, researcher, artist, and game-maker. I care about socially impactful tech, experimental new media, creative tools, and <PlayLink type="hobbies" />.
-        <br /><br />
-        Reach out anytime at <b>tiffanywang at berkeley dot edu</b>.
-        <br /><br />
-        <span className="flex flex-col sm:flex-row sm:items-center">
-          Else, find me at:
-          <span className="flex flex-wrap items-center sm:ml-2 gap-2">
-            <Url href="https://www.linkedin.com/in/wtiffany/">linkedin</Url>
-            <Url href="https://github.com/cnnmon">github</Url>
-            <Url href="https://cnnmon.itch.io/">itch.io</Url>
-            <Url href="https://twitter.com/cnnmonie">twitter</Url>
-            <Url href="https://scholar.google.com/citations?hl=en&user=p8hhfi4AAAAJ">gscholar</Url>
-          </span>
+        find me at
+        {" {"}<span className="mx-1">
+          <Url href="https://www.linkedin.com/in/wtiffany/">linkedin</Url>{", "}
+          <Url href="https://github.com/cnnmon">github</Url>{", "}
+          <Url href="https://cnnmon.itch.io/">itch.io</Url>{", "}
+          <Url href="https://twitter.com/cnnmonie">twitter</Url>{", "}
+          <Url href="https://scholar.google.com/citations?hl=en&user=p8hhfi4AAAAJ">gscholar</Url>
         </span>
-        <br />
+        {"}"}
+        {" "}. ݁₊ ⊹ . ݁ ⟡ ݁ . ⊹ ₊ ݁.
       </p>
-    </>
+    </div>
   )
 }
 
