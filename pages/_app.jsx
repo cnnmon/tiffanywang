290efcb8699537c31eedf306155a96841b48e989
cnnmon <tiffanywang@berkeley.edu>
2025-08-10 00:11:19 -0700

    Revert "wip"
    This reverts commit d291378e69104a858176cfa6543abde14c711290.

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,4 +1,5 @@
 import '../styles/globals.css'
+import logo from '../public/logo.svg'
 import Image from 'next/image'
 import Link from 'next/link'
 import Head from 'next/head'
@@ -22,31 +23,29 @@ function MyApp({ Component, pageProps }) {
   }
 
   return (
-    <div className="flex flex-col w-full gap-8 p-4">
+    <div className="flex flex-col justify-center w-full md:max-w-3xl mx-auto gap-8 mt-[8vh] p-4">
       <Head>
         <title>tiffanywang</title>
       </Head>
-      <div className="fixed inset-0 flex items-center justify-center pointer-events-none" style={{ zIndex: -1 }}>
-        <Image src="/cabbage.png" width={500} height={500} alt="cabbage background" className="w-full max-w-2xl opacity-50" />
-      </div>
-      <div className="flex gap-5 md:flex-row">
-        <nav className="flex flex-col md:w-1/7 text-right ">
+      <Image src={logo} alt="logo" className="w-[150px] md:w-1/5 px-6" />
+      <div className="flex flex-row gap-5 md:flex-row flex-col">
+        <nav className="flex md:w-1/7 md:flex-col gap-2 text-right text-lg">
           {pages.map((page, index) => <NavItem key={index} path={page.path} title={page.title} />)}
         </nav>
-        <div className="w-full max-w-2xl body">
+        <div className="md:w-4/5 body">
           <AnimatePresence exitBeforeEnter mode="wait">
             <motion.div
               key={router.route}
-              initial={{ opacity: 0 }}
-              animate={{ opacity: 1 }}
-              exit={{ opacity: 0 }}
+              initial={{ opacity: 0, y: 20 }}
+              animate={{ opacity: 1, y: 0 }}
+              exit={{ opacity: 0, y: 20 }}
               transition={{ duration: 0.3 }}
             >
               <Component {...pageProps} />
             </motion.div>
           </AnimatePresence>
-          <footer className="fixed bottom-0 left-0 right-0 p-4 text-center text-sm">
-            <p>•───────• built with <a className="link" href="https://github.com/cnnmon/tiffanywang"><b>˙ᵕ˙</b></a> •───────•</p>
+          <footer className="p-4 text-center text-sm">
+            <p>•───────• Built with <a className="link" href="https://github.com/cnnmon/tiffanywang"><b>˙ᵕ˙</b></a> •───────•</p>
           </footer>
         </div>
       </div>
